c2914a9bcd38f83c3b46d9572aa2d6d3f6f74e54
brlnd <andrea.berlanda92@gmail.com>
2018-05-17 17:47:11 +0200

    Chapter 2 -3

---
+++ Part1.py
@@ -0,0 +1,21 @@
+#Chapter 2: Invoke the Interpreter
+the_world_is_flat = True
+if the_world_is_flat:
+    print("Be careful not to fall off!")
+
+#Suggestion: declare which is the source of the enconding, if not the default one (UTF-8)
+
+#Chapter 3: An Informal Introduction to Python
+#3.1 Using Python as a Calculator
+#3.1.1 Numbers
+2+2
+50-5*6
+(50-5*6)/4
+8/5
+17/3
+17//3 #discard the fractional part
+17%3 #returns the remainder
+5**2 #power operator
+width=57
+height = 89
+width*height
